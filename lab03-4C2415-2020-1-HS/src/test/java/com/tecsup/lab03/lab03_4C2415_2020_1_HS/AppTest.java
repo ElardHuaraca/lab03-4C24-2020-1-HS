846e477b1d16034daf232ba6e55a9cf0ab0d7f2a
Elard <elard.sd.9@gmail.com>
2020-05-16 17:24:22 -0500

    primer comit

---
+++ lab03-4C2415-2020-1-HS/src/test/java/com/tecsup/lab03/lab03_4C2415_2020_1_HS/AppTest.java
@@ -0,0 +1,20 @@
+package com.tecsup.lab03.lab03_4C2415_2020_1_HS;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
